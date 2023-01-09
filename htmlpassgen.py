6f9f9e42f5e660611a4f370a2a0e8f323f27bbcc
inori010 <zhilin010@icloud.com>
2023-01-09 15:49:47 -0500

    Modify index.html to run external python script.

--- htmlpassgen.py
+++ htmlpassgen.py
@@ -1,13 +1,15 @@
-from js import a
 from js import document
 from pyodide import create_proxy
 import secrets
 import string
-
-def create_password(a):
+    
+def create_password(e):
+    global password
+    from js import a
     library = string.ascii_letters + string.digits + "!@#$%&-/"
-    password = "".join(secrets.choice(library) for i in range(a))
+    password = "".join(secrets.choice(library) for i in range(int(a)))
+    pyscript.write("generated-password", password)
 
-    create_password_proxy = create_proxy(create_password)
-    create_password_button = document.getElementbyClassName("generate-button")
-    create_password_button.addEventListener("click", create_password_proxy)
+create_password_proxy = create_proxy(create_password)
+create_password_button = document.getElementById('generate-button')
+create_password_button.addEventListener("click", create_password_proxy)
